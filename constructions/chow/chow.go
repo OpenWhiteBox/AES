0c13420a02077f1ff542b0eb7c9c560069191b97
Brendan McMillion <brendan@cloudflare.com>
2015-09-22 12:27:15 -0700

    Tabularize XORs.

--- constructions/chow/chow.go
+++ constructions/chow/chow.go
@@ -16,11 +16,36 @@ func (tbox TBox) Get(i byte) byte {
 	return tbox.Constr.SubByte(i^tbox.KeyByte1) ^ tbox.KeyByte2
 }
 
-type Construction struct {
-	TBox [10][16]table.ByteTable
+type TyiTable uint
+
+func (tyi TyiTable) Get(i byte) (out uint32) {
+	// Calculate dot product of i and [0x02 0x01 0x01 0x03]
+	j := number.ByteFieldElem(i)
+
+	a := number.ByteFieldElem(2).Mul(j)
+	b := number.ByteFieldElem(1).Mul(j)
+	c := number.ByteFieldElem(3).Mul(j)
+
+	// Merge into one output and rotate according to column.
+	out = (uint32(a) << 24) | (uint32(b) << 16) | (uint32(b) << 8) | uint32(c)
+	out = (out >> (8 * uint(tyi))) | (out << (32 - 8*uint(tyi)))
+
+	return
 }
 
-func GenerateTables(key [16]byte) (table [10][16]table.ByteTable) {
+type XORTable struct{}
+
+func (xor XORTable) Get(i byte) (out byte) {
+	return (i >> 4) ^ (i & 0xf)
+}
+
+type Construction struct {
+	TBoxTyiTable [9][16]table.WordTable         // [round][position]
+	TBox         [16]table.ByteTable            // [position]
+	XORTable     [9][16][3][2]table.NibbleTable // [round][position][level][side]
+}
+
+func GenerateTables(key [16]byte) (tyi [9][16]table.WordTable, tbox [16]table.ByteTable, xor [9][16][3][2]table.NibbleTable) {
 	constr := saes.Construction{key}
 	roundKeys := constr.StretchedKey()
 
@@ -29,36 +54,78 @@ func GenerateTables(key [16]byte) (table [10][16]table.ByteTable) {
 		roundKeys[k] = constr.ShiftRows(roundKeys[k])
 	}
 
-	// Build T-Boxes 1 to 9
+	// Build T-Boxes and Tyi Tables 1 to 9
 	for round := 0; round < 9; round++ {
-		for place := 0; place < 16; place++ {
-			table[round][place] = TBox{constr, roundKeys[round][place], 0}
+		for pos := 0; pos < 16; pos++ {
+			tyi[round][pos] = table.ComposedToWordTable{
+				TBox{constr, roundKeys[round][pos], 0},
+				TyiTable(pos % 4),
+			}
 		}
 	}
 
 	// 10th T-Box
-	for place := 0; place < 16; place++ {
-		table[9][place] = TBox{constr, roundKeys[9][place], roundKeys[10][place]}
+	for pos := 0; pos < 16; pos++ {
+		tbox[pos] = TBox{constr, roundKeys[9][pos], roundKeys[10][pos]}
+	}
+
+	// Generate XOR Tables
+	for round := 0; round < 9; round++ {
+		for pos := 0; pos < 16; pos++ {
+			for level := 0; level < 3; level++ {
+				for side := 0; side < 2; side++ {
+					xor[round][pos][level][side] = XORTable{}
+				}
+			}
+		}
 	}
 
 	return
 }
 
 func (constr *Construction) Encrypt(block [16]byte) [16]byte {
-	for i := 0; i < 9; i++ {
+	for round := 0; round < 9; round++ {
 		block = constr.shiftRows(block)
 
-		for j := 0; j < 16; j++ {
-			block[j] = constr.TBox[i][j].Get(block[j])
-		}
+		for pos := 0; pos < 16; pos += 4 {
+			// Expand one word of the plaintext with the T-Boxes composed with Tyi Tables.
+			a := constr.TBoxTyiTable[round][pos+0].Get(block[pos+0])
+			b := constr.TBoxTyiTable[round][pos+1].Get(block[pos+1])
+			c := constr.TBoxTyiTable[round][pos+2].Get(block[pos+2])
+			d := constr.TBoxTyiTable[round][pos+3].Get(block[pos+3])
 
-		block = constr.mixColumns(block)
+			// Squash expanded plaintext into one word with 3 pairwise XORs (calc'd one nibble at a time) -- (a ^ b) ^ (c ^ d)
+			var ad uint32 = 0
+
+			for realPos := 0; realPos < 4; realPos++ {
+				for side := 0; side < 2; side++ { // side, as in left or right side of the byte.
+					abPartial := byte(((a & 0xf) << 4) | (b & 0xf))
+					cdPartial := byte(((c & 0xf) << 4) | (d & 0xf))
+
+					ab := uint32(constr.XORTable[round][realPos][0][side].Get(abPartial)) // (a ^ b)
+					cd := uint32(constr.XORTable[round][realPos][1][side].Get(cdPartial)) // (c ^ d)
+
+					adPartial := byte((ab << 4) | cd)
+
+					offset := uint(4 * (2*realPos + side))
+					ad |= uint32(constr.XORTable[round][realPos][2][side].Get(adPartial)) << offset // (a ^ b) ^ (c ^ d)
+
+					a, b, c, d = a>>4, b>>4, c>>4, d>>4
+				}
+			}
+
+			// Split finished word into 4 bytes and put it away.
+			block[pos+0] = byte(ad >> 24)
+			block[pos+1] = byte(ad >> 16)
+			block[pos+2] = byte(ad >> 8)
+			block[pos+3] = byte(ad)
+		}
 	}
 
 	block = constr.shiftRows(block)
 
-	for j := 0; j < 16; j++ {
-		block[j] = constr.TBox[9][j].Get(block[j])
+	for pos := 0; pos < 16; pos++ {
+		block[pos] = constr.TBox[pos].Get(block[pos])
 	}
 
 	return block
@@ -69,31 +136,4 @@ func (constr *Construction) shiftRows(block [16]byte) [16]byte {
 		block[0], block[5], block[10], block[15], block[4], block[9], block[14], block[3], block[8], block[13], block[2],
 		block[7], block[12], block[1], block[6], block[11],
 	}
-}
-
-func (constr *Construction) mixColumns(block [16]byte) (out [16]byte) {
-	for i := 0; i < 4; i++ {
-		copy(out[4*i:4*(i+1)], constr.mixColumn(block[4*i:4*(i+1)]))
-	}
-
-	return out
-}
-
-func (constr *Construction) mixColumn(slice []byte) []byte {
-	column := number.ArrayFieldElem{}
-	for i := 0; i < 4; i++ {
-		column = append(column, number.ByteFieldElem(slice[i]))
-	}
-
-	column = column.Mul(number.ArrayFieldElem{
-		number.ByteFieldElem(0x02), number.ByteFieldElem(0x01),
-		number.ByteFieldElem(0x01), number.ByteFieldElem(0x03),
-	})
-
-	out := make([]byte, 4)
-	for i := 0; i < len(column); i++ {
-		out[i] = byte(column[i])
-	}
-
-	return out
 }
--- constructions/chow/chow_test.go
+++ constructions/chow/chow_test.go
@@ -5,13 +5,14 @@ import (
 	"testing"
 )
 
-var key = GenerateTables([16]byte{72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33, 33, 33, 33, 33})
+var key = [16]byte{72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33, 33, 33, 33, 33}
 
 func TestShiftRows(t *testing.T) {
 	in := [16]byte{99, 202, 183, 4, 9, 83, 208, 81, 205, 96, 224, 231, 186, 112, 225, 140}
 	out := [16]byte{99, 83, 224, 140, 9, 96, 225, 4, 205, 112, 183, 81, 186, 202, 208, 231}
 
-	constr := Construction{key}
+	tboxtyi, tbox, xor := GenerateTables(key)
+	constr := Construction{tboxtyi, tbox, xor}
 	cand := constr.shiftRows(in)
 
 	for i := 0; i < 16; i++ {
@@ -21,12 +22,21 @@ func TestShiftRows(t *testing.T) {
 	}
 }
 
-func TestMixColumns(t *testing.T) {
+func TestTyiTable(t *testing.T) {
 	in := [16]byte{99, 83, 224, 140, 9, 96, 225, 4, 205, 112, 183, 81, 186, 202, 208, 231}
 	out := [16]byte{95, 114, 100, 21, 87, 245, 188, 146, 247, 190, 59, 41, 29, 185, 249, 26}
 
-	constr := Construction{key}
-	cand := constr.mixColumns(in)
+	a, b, c, d := TyiTable(0), TyiTable(1), TyiTable(2), TyiTable(3)
+	cand := [16]byte{}
+
+	for i := 0; i < 16; i += 4 {
+		x := a.Get(in[i+0]) ^ b.Get(in[i+1]) ^ c.Get(in[i+2]) ^ d.Get(in[i+3])
+
+		cand[i+0] = byte(x >> 24)
+		cand[i+1] = byte(x >> 16)
+		cand[i+2] = byte(x >> 8)
+		cand[i+3] = byte(x)
+	}
 
 	for i := 0; i < 16; i++ {
 		if out[i] != cand[i] {
@@ -37,8 +47,8 @@ func TestMixColumns(t *testing.T) {
 
 func TestEncrypt(t *testing.T) {
 	for n, vec := range test_vectors.AESVectors {
-		table := GenerateTables(vec.Key)
-		constr := Construction{table}
+		tboxtyi, tbox, xor := GenerateTables(vec.Key)
+		constr := Construction{tboxtyi, tbox, xor}
 		cand := constr.Encrypt(vec.In)
 
 		for i := 0; i < 16; i++ {
