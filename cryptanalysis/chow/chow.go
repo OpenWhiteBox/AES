7374b844d35cdf76e54ad53bc7923e1b07af842b
Brendan McMillion <brendan@cloudflare.com>
2015-10-08 13:29:46 -0700

    Update cryptanalysis to use new API.

--- cryptanalysis/chow/chow.go
+++ cryptanalysis/chow/chow.go
@@ -2,6 +2,8 @@ package chow
 
 import (
 	"github.com/OpenWhiteBox/AES/constructions/chow"
+	"github.com/OpenWhiteBox/AES/primitives/encoding"
+	"github.com/OpenWhiteBox/AES/primitives/matrix"
 	"github.com/OpenWhiteBox/AES/primitives/table"
 )
 
@@ -28,58 +30,87 @@ func (f F) Get(i byte) byte {
 	pos := f.Position / 4 * 4
 
 	block := make([]byte, 4)
-	block[0], block[1] = i, f.Base
+	copy(block, []byte{i, 0, 0, f.Base})
 
 	stretched := f.Constr.ExpandWord(f.Constr.TBoxTyiTable[f.Round][pos:pos+4], block)
-	copy(block, f.Constr.SquashWords(f.Constr.HighXORTable[f.Round][2*pos:2*pos+8], stretched))
+	f.Constr.SquashWords(f.Constr.HighXORTable[f.Round][2*pos:2*pos+8], stretched, block)
 
 	stretched = f.Constr.ExpandWord(f.Constr.MBInverseTable[f.Round][pos:pos+4], block)
-	copy(block, f.Constr.SquashWords(f.Constr.LowXORTable[f.Round][2*pos:2*pos+8], stretched))
+	f.Constr.SquashWords(f.Constr.LowXORTable[f.Round][2*pos:2*pos+8], stretched, block)
 
 	return block[f.Position%4]
 }
 
 // Qtilde is the approximation of the RoundEncoding between two rounds.
 type Qtilde struct {
-  S [][256]byte
+	S [][256]byte
 }
 
 func (q Qtilde) Encode(i byte) byte {
-  return q.S[i][0]
+	return q.S[i][0]
 }
 
 func (q Qtilde) Decode(i byte) byte {
-  for j, perm := range q.S {
-    if perm[0] == i {
-      return byte(j)
-    }
-  }
+	for j, perm := range q.S {
+		if perm[0] == i {
+			return byte(j)
+		}
+	}
 
-  return byte(0)
+	return byte(0)
 }
 
 func RecoverKey(constr chow.Construction) (key [16]byte) {
-	// The set of elements S, of the form fXX(f00^(-1)(x)) = Q(Q^(-1)(x) + b) for indeterminate x is isomorphic to the
-	// additive group (GF(2)^8, xor) under composition.
+	S := GenerateS(constr, 0, 0)
+	_ = FindBasisAndSort(S)
 
-	// Find 8 elements of S that act as a basis for S and build isomorphism psi.
+	qtilde := Qtilde{S}
 
-	// Build Qtilde, with the identity g(00) = Qtilde(psi(g)), foreach g in S.
+	constr.TBoxTyiTable[0][0] = encoding.WordTable{
+		encoding.IdentityByte{},
+		encoding.ConcatenatedWord{
+			qtilde,
+			encoding.IdentityByte{},
+			encoding.IdentityByte{},
+			encoding.IdentityByte{},
+		},
+		constr.TBoxTyiTable[1][0],
+	}
 
 	return
 }
 
+func DecomposeAffineEncoding(e encoding.Byte) (matrix.Matrix, byte) {
+	m := matrix.Matrix{
+		matrix.Row{0}, matrix.Row{0}, matrix.Row{0}, matrix.Row{0},
+		matrix.Row{0}, matrix.Row{0}, matrix.Row{0}, matrix.Row{0},
+	}
+	c := e.Encode(0)
+
+	for i := uint(0); i < 8; i++ {
+		x := e.Encode(1<<i) ^ c
+
+		for j := uint(0); j < 8; j++ {
+			if (x>>j)&1 == 1 {
+				m[j][0] += 1 << i
+			}
+		}
+	}
+
+	return m, c
+}
+
 // The set of elements S, of the form fXX(f00^(-1)(x)) = Q(Q^(-1)(x) + b) for indeterminate x is isomorphic to the
 // additive group (GF(2)^8, xor) under composition.
-func GenerateS(constr chow.Construction) [][256]byte {
-	f00 := InvertibleTable(F{constr, 0, 0, 0x00})
+func GenerateS(constr chow.Construction, round, pos int) [][256]byte {
+	f00 := InvertibleTable(F{constr, round, pos, 0x00})
 	f00Inv := Invert(f00)
 
 	S := make([][256]byte, 256)
 	for x := 0; x < 256; x++ {
 		copy(S[x][:], table.SerializeByte(table.ComposedBytes{
-			F{constr, 0, 0, byte(x)},
 			f00Inv,
+			F{constr, round, pos, byte(x)},
 		}))
 	}
 
@@ -97,7 +128,7 @@ func FindBasisAndSort(S [][256]byte) (basis []table.Byte) {
 			copy(vect[:], table.SerializeByte(FunctionFromBasis(i, basis)))
 
 			// Move it to the correct position in S.
-			for j := i + 1; j < len(S); j++ {
+			for j := i; j < len(S); j++ {
 				if vect == S[j] {
 					S[i], S[j] = S[j], S[i]
 					break
--- cryptanalysis/chow/chow_test.go
+++ cryptanalysis/chow/chow_test.go
@@ -5,15 +5,17 @@ import (
 	"testing"
 
 	"github.com/OpenWhiteBox/AES/constructions/chow"
+	"github.com/OpenWhiteBox/AES/primitives/encoding"
+	"github.com/OpenWhiteBox/AES/primitives/matrix"
 	"github.com/OpenWhiteBox/AES/primitives/table"
 
 	test_vectors "github.com/OpenWhiteBox/AES/constructions/test"
 )
 
-func testConstruction() (chow.Construction, [16]byte) {
+func testConstruction() (chow.Construction, []byte) {
 	key := test_vectors.AESVectors[50].Key
 	seed := test_vectors.AESVectors[51].Key
-	constr, _, _ := chow.GenerateKeys(key, seed)
+	constr, _, _ := chow.GenerateKeys(key, seed, chow.SameMasks(chow.IdentityMask))
 
 	return constr, key
 }
@@ -25,7 +27,7 @@ func TestRecoverKey(t *testing.T) {
 
 func TestFindBasisAndSort(t *testing.T) {
 	constr, _ := testConstruction()
-	S := GenerateS(constr)
+	S := GenerateS(constr, 0, 0)
 
 	basis := FindBasisAndSort(S)
 
@@ -56,16 +58,44 @@ func TestFindBasisAndSort(t *testing.T) {
 func TestQtilde(t *testing.T) {
 	constr, _ := testConstruction()
 
-	S := GenerateS(constr)
+	S := GenerateS(constr, 0, 0)
 	_ = FindBasisAndSort(S)
 
-  qtilde := Qtilde{S}
+	qtilde := Qtilde{S}
 
-	for i := 0; i < 256; i++ {
+	for i := 0; i < 256; i++ { // Test first identity, that f = Qtilde <- ...
 		cand := qtilde.Encode(qtilde.Decode(byte(i)) ^ 37)
 
 		if S[37][i] != cand {
 			t.Fatalf("Identity broken! f = Qtilde <- xor psi <- Qtilde^(-1)\nf(%v) = %v, g(%v) = %v", i, S[37][i], i, cand)
 		}
-  }
+	}
+}
+
+func TestDecomposeAffineEncoding(t *testing.T) {
+	ae := AffineEncoding{
+		matrix.Matrix{
+			matrix.Row{0xA4},
+			matrix.Row{0x49},
+			matrix.Row{0x92},
+			matrix.Row{0x25},
+			matrix.Row{0x4a},
+			matrix.Row{0x94},
+			matrix.Row{0x29},
+			matrix.Row{0x52},
+		},
+		0x63,
+	}
+
+	m, c := DecomposeAffineEncoding(ae)
+
+	for i := 0; i < 8; i++ {
+		if ae.M[i][0] != m[i][0] {
+			t.Fatalf("Row %v in the linear part is wrong! %v != %v", i, ae.M[i][0], m[i][0])
+		}
+	}
+
+	if ae.C != c {
+		t.Fatalf("The affine part is wrong! %v != %v", ae.C, c)
+	}
 }
