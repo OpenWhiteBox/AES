9dd2bd800c3ca1e96c0078b867ef83e6cbc9aba3
Brendan McMillion <brendan@cloudflare.com>
2015-11-12 10:41:41 -0800

    Add common.RandomSource.{Dirichlet, Monotone}. - Improve some documentation.

--- constructions/common/random.go
+++ constructions/common/random.go
@@ -48,8 +48,8 @@ func (rs *RandomSource) subKey(label []byte) []byte {
 	return subKey
 }
 
-// Stream takes a (private) seed and a (possibly public) label and produces an io.Reader giving random bytes, useful for
-// deterministically generating random matrices/encodings, in place of (crypto/rand).Reader.
+// Stream takes a (possibly public) label and produces an io.Reader giving random bytes, useful for deterministically
+// generating random matrices/encodings, in place of (crypto/rand).Reader.
 //
 // It does this by using the seed as an AES key and the label as the IV in CTR mode.  The io.Reader is providing the
 // AES-CTR encryption of /dev/null.
@@ -66,7 +66,7 @@ func (rs *RandomSource) Stream(label []byte) io.Reader {
 	return stream
 }
 
-// Shuffle takes a (private) seed and a (possibly public) label and produces a random shuffle of the integers [0, 16).
+// Shuffle takes a (possibly public) label and produces a random shuffle of the integers [0, 16).
 func (rs *RandomSource) Shuffle(label []byte) encoding.Shuffle {
 	key := [16]byte{}
 	copy(key[:], label)
@@ -81,7 +81,7 @@ func (rs *RandomSource) Shuffle(label []byte) encoding.Shuffle {
 	}
 }
 
-// Matrix takes a (private) seed and a (possibly public) label and produces a random non-singular 128x128 matrix.
+// Matrix takes a (possibly public) label and produces a random non-singular 128x128 matrix.
 func (rs *RandomSource) Matrix(label []byte, size int) matrix.Matrix {
 	key := [16]byte{}
 	copy(key[:], label)
@@ -94,4 +94,64 @@ func (rs *RandomSource) Matrix(label []byte, size int) matrix.Matrix {
 		rs.matrixCache[key] = matrix.GenerateRandom(rs.Stream(label), size)
 		return rs.matrixCache[key]
 	}
+}
+
+// Dirichlet takes a (possibly public) label and produces the output of a uniform dirichlet distribution with `length`
+// variables, summing to `sum`.
+func (rs *RandomSource) Dirichlet(label []byte, length, sum int) []int {
+	if length == 0 && sum != 0 {
+		panic("Dirichlet: Can't sample distribution of zero variables and get a non-zero sum!")
+	}
+
+	out := make([]int, length)
+	s := rs.Stream(label)
+
+	// Typical way of sampling a Dirichlet distribution:
+	// http://stats.stackexchange.com/questions/69210/drawing-from-dirichlet-distribution
+	// Takes multiple guesses because of rounding errors.
+	guess := func() {
+		buff := make([]byte, length)
+		s.Read(buff)
+
+		candSum := 0
+		for pos := 0; pos < length; pos++ {
+			out[pos] = int(buff[pos])
+			candSum += out[pos]
+		}
+
+		for pos := 0; pos < length; pos++ {
+			out[pos] = out[pos] * sum
+
+			r := out[pos] % candSum
+			out[pos] = out[pos] / candSum
+
+			if r >= candSum/2 {
+				out[pos]++
+			}
+		}
+	}
+
+	candSum := 0
+	for candSum != sum {
+		candSum = 0
+		guess()
+
+		for _, x := range out {
+			candSum += x
+		}
+	}
+
+	return out
+}
+
+// Monotone takes a (possibly public) label and produces a random monotone function which is `length` units long and
+// maximizes at `max`.
+func (rs *RandomSource) Monotone(label []byte, length, max int) []int {
+	out := rs.Dirichlet(label, length, max)
+
+	for i := 1; i < length; i++ {
+		out[i] += out[i-1]
+	}
+
+	return out
 }
