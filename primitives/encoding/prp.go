1d833202f53416dcb10908bd2d1c35d8a6ade4b4
Brendan McMillion <brendan@cloudflare.com>
2015-12-14 14:14:51 -0800

    Add random SBox generation to primitives/encoding.

--- primitives/encoding/prp.go
+++ primitives/encoding/prp.go
@@ -35,3 +35,32 @@ func GenerateShuffle(reader io.Reader) (s Shuffle) {
 
 	return
 }
+
+type SBox struct {
+	EncKey, DecKey [256]byte
+}
+
+func (s SBox) Encode(i byte) byte {
+	return s.EncKey[i]
+}
+
+func (s SBox) Decode(i byte) byte {
+	return s.DecKey[i]
+}
+
+func GenerateSBox(reader io.Reader) (s SBox) {
+	for i := 0; i < 256; i++ {
+		s.EncKey[i] = byte(i)
+	}
+
+	for i := int64(255); i > 0; i-- {
+		j, _ := rand.Int(reader, big.NewInt(i+1))
+		s.EncKey[i], s.EncKey[j.Int64()] = s.EncKey[j.Int64()], s.EncKey[i]
+	}
+
+	for i, j := range s.EncKey {
+		s.DecKey[j] = byte(i)
+	}
+
+	return
+}
