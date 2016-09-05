054df2a1cf6bc5dadc1181b7001f981f21c06ee3
Brendan McMillion <brendan@cloudflare.com>
2016-09-05 08:21:08 -0700

    Initial implementation of toy construction.

---
+++ constructions/toy/toy.go
@@ -0,0 +1,48 @@
+// Package toy implements the toy white-box AES construction, based on simple SPN disambiguation.
+package toy
+
+import (
+	"github.com/OpenWhiteBox/primitives/encoding"
+	"github.com/OpenWhiteBox/primitives/number"
+)
+
+type Construction [11]encoding.BlockAffine
+
+// BlockSize returns the block size of AES. (Necessary to implement cipher.Block.)
+func (constr Construction) BlockSize() int { return 16 }
+
+// Encrypt encrypts the first block in src into dst. Dst and src may point at the same memory.
+func (constr Construction) Encrypt(dst, src []byte) {
+	state := [16]byte{}
+	copy(state[:], src[:])
+
+	state = constr[0].Encode(state)
+
+	for round := 1; round < 11; round++ {
+		for pos := 0; pos < 16; pos++ {
+			state[pos] = byte(number.ByteFieldElem(state[pos]).Invert())
+		}
+
+		state = constr[round].Encode(state)
+	}
+
+	copy(dst[:], state[:])
+}
+
+// Decrypt decrypts the first block in src into dst. Dst and src may point at the same memory.
+func (constr Construction) Decrypt(dst, src []byte) {
+	state := [16]byte{}
+	copy(state[:], src[:])
+
+	state = constr[10].Decode(state)
+
+	for round := 9; round >= 0; round-- {
+		for pos := 0; pos < 16; pos++ {
+			state[pos] = byte(number.ByteFieldElem(state[pos]).Invert())
+		}
+
+		state = constr[round].Decode(state)
+	}
+
+	copy(dst[:], state[:])
+}
---
+++ constructions/toy/toy_test.go
@@ -0,0 +1,54 @@
+package toy
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/OpenWhiteBox/primitives/matrix"
+
+	"github.com/OpenWhiteBox/AES/constructions/common"
+
+	test_vectors "github.com/OpenWhiteBox/AES/constructions/test"
+)
+
+func TestEncrypt(t *testing.T) {
+	for n, vec := range test_vectors.GetAESVectors(testing.Short()) {
+		constr, inputMask, outputMask := GenerateKeys(
+			vec.Key, vec.Key, common.IndependentMasks{common.RandomMask, common.RandomMask},
+		)
+
+		in, out := [16]byte{}, [16]byte{}
+
+		copy(in[:], matrix.Row(vec.In))
+		in = inputMask.Decode(in) // Apply input encoding.
+
+		constr.Encrypt(out[:], in[:])
+
+		out = outputMask.Decode(out) // Remove output encoding.
+
+		if !bytes.Equal(vec.Out, out[:]) {
+			t.Fatalf("Real disagrees with result in test vector %v! %x != %x", n, vec.Out, out)
+		}
+	}
+}
+
+func TestDecrypt(t *testing.T) {
+	for n, vec := range test_vectors.GetAESVectors(testing.Short()) {
+		constr, inputMask, outputMask := GenerateKeys(
+			vec.Key, vec.Key, common.IndependentMasks{common.RandomMask, common.RandomMask},
+		)
+
+		in, out := [16]byte{}, [16]byte{}
+
+		copy(in[:], matrix.Row(vec.Out))
+		in = outputMask.Encode(in) // Apply output encoding.
+
+		constr.Decrypt(out[:], in[:])
+
+		out = inputMask.Encode(out) // Remove input encoding.
+
+		if !bytes.Equal(vec.In, out[:]) {
+			t.Fatalf("Real disagrees with result in test vector %v! %x != %x", n, vec.Out, out)
+		}
+	}
+}
